118a47c72541f98d6fc23bd7ae0aa452528ac6fa
myrelii <164705004+myrelii@users.noreply.github.com>
2024-12-09 23:56:30 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+import { flags } from './flags.js';
+
+const flagsContainer = document.getElementById('flags-container');
+
+flags.forEach(flag => {
+ 
+  const flagWrapper = document.createElement('/div');
+  flagWrapper.classList.add('flag-wrapper');
+
+  
+  const imgElement = document.createElement('img');
+  imgElement.src = flag.image;
+  imgElement.alt = flag.name; 
+  imgElement.id = flag.id;    
+
+ 
+  const nameElement = document.createElement('/p');
+  nameElement.textContent = flag.nome;
+
+ 
+  flagWrapper.appendChild(imgElement);
+  flagWrapper.appendChild(nameElement);
+
+  
+  flagsContainer.appendChild(flagWrapper);
+});
